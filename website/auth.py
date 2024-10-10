2d39f1625f870e583a3869143af6ec3e669dfd2a
cameron_morgan <cwm36@duke.edu>
2024-10-10 15:01:49 -0400

    "everything is working"

--- website/auth.py
+++ website/auth.py
@@ -19,7 +19,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('pick_home.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -62,6 +62,6 @@ def sign_up():
             db.session.commit() #user has been added to the db 
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('pick_home.home'))
 
     return render_template("sign_up.html", user=current_user)
