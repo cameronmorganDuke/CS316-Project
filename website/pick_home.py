5b0dc559bba0459ee234a4fa33569d4c64bc9e5d
cameron_morgan <cameron.morgan@duke.edu>
2024-11-12 09:53:43 -0500

    Revert "Merge pull request #7 from cameronmorganDuke/JPsBranch"
    This reverts commit f5e75345483b1c759304980f4bb3ff9a25fcda5e, reversing
    changes made to ca88e469e86c79149a2975381991b9a241c47b68.

--- website/pick_home.py
+++ website/pick_home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, session
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, session, flash
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -7,73 +7,61 @@ from .utils import *
 from sqlalchemy import *
 
 pick_home = Blueprint('pick_home', __name__)
-address = ""
-city = ""
-state = ""
-zip_code = ""
-country = ""
-change_input_one = ""
-change_input_two = ""
 
 @pick_home.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        global address, city, state, zip_code, country
         address = request.form.get('address')
-        session['address'] = request.form.get('address')
-        session['city'] = request.form.get('city')
-        session['state'] = request.form.get('state')
-        session['zip_code'] = request.form.get('zip')  # 'zip' matches the name attribute
-        session['country'] = request.form.get('country')
-        print(address, city, state, zip_code, country)
-        
-        
-        
-        # After processing the POST request, render display_home.html
-        return redirect(url_for('pick_home.home_info'))
+        return redirect(url_for('pick_home.home_info', address=address))
 
-    # If it's a GET request, render the initial form
     return render_template("get_home.html", user=current_user)
 
-@pick_home.route('/display_home', methods=['GET', 'POST'])
-def home_info():
-    global address, city, state, zip_code, country
-    
-    street_number, street_name = extract_address_components(session['address'])
+@pick_home.route('/<address>', methods=['GET', 'POST'])
+def home_info(address):
+    street_number, street_name = extract_address_components(address)
     street_number = street_number.strip()
     street_name = street_name.upper().strip()
-    city_name = session['city'].upper().strip()
-    
     query = f"""
-        SELECT ZONING, GROSS_LEASABLE_AREA, TOTAL_PROP_VALUE, Total_Bedrooms, ETJ, CALCULATED_ACRES
+        SELECT ZONING, GROSS_LEASABLE_AREA, TOTAL_PROP_VALUE, Total_Bedrooms, ETJ, CALCULATED_ACRES, NEIGHBORHOOD
         FROM dtarp
         WHERE TRIM(street)='{street_name}' and number='{street_number}'
     """
-        # WHERE TRIM(street)='{street_name}' and TRIM(number)='{street_number}'
     
     result = db.session.execute(text(query))
     rows = result.fetchall()
     
     print(f"\n\n{len(rows)}\n\n")
     
-    assert len(rows) != 0, "There are no entries in the database for this address"
-    assert len(rows) == 1, f"There are {len(rows)} entries in the database for this address"
-    
-    zoning, sqft, property_value, num_beds, etj, acres = rows[0]
+    if len(rows) != 1:
+        flash(f"There are {len(rows)} entries in the database for this address", 'error')
+        return redirect(url_for('pick_home.home'))
+            
+    zoning, sqft, property_value, num_beds, etj, acres, neighborhood = rows[0]
+    property_value = property_value if property_value else 1
     
     assert zoning != None and sqft != None and property_value != None and num_beds != None and etj != None and acres, "Not all values are initialized"
     
-    monthly_rent = property_value*0.015
-    annual_rental_income = monthly_rent*12
+    sqft = 625*num_beds if not sqft else sqft
+    
+    monthly_rent = int(property_value*0.005)
     
     vacancy_allowance_pct = 5/100
     
-    annual_rental_income = annual_rental_income - annual_rental_income*vacancy_allowance_pct
+    annual_rental_income = monthly_rent * 12 * (1 - vacancy_allowance_pct)
     
     num_units = estimate_units(zoning, acres)
     
     isGas = False
+    
+    utilities = estimate_heating_cost(isGas, sqft) + \
+        estimate_garbage_cost(num_units, include=True) + \
+        estimate_water_cost(num_beds, water_usage_per_occupant=sum(WATER_USAGE_PER_OCCUPANT)/len(WATER_USAGE_PER_OCCUPANT)) + \
+        calculate_lawn_snow_cost(acres, lawn_care_per_acre=sum(LAWN_CARE_PER_ACRE)/len(LAWN_CARE_PER_ACRE))
+    
+    utilities = round(utilities, 0)
+    
+    legal = round(estimate_legal_cost(annual_rental_income, num_units), 0)
     
     expenses = estimate_expenses(
         annual_rental_income, num_units, 
@@ -85,26 +73,36 @@ def home_info():
     # annual gross scheduled income = total monthly rental income * 12
     # gross operating income = annual gross scheduled income - vacancy allowance % * annual gross scheduled income
 
-    
     #create function to get Cap Rate and NOI 
     noi = annual_rental_income - expenses
     cap_rate = noi / property_value * 100
-    
-    print('zoning', 'sqft', 'property_value', 'num_beds', 'etj', 'acres')
-    print(zoning, sqft, property_value, num_beds, etj, acres)
-    print()
-    print("monthly rent", monthly_rent)
-    print("rental income", annual_rental_income)
-    print("expenses", expenses)
-    print("noi", noi)
-    print("cap rate", cap_rate)
-    
+        
     selected_value = request.form.get('selected_value')
     print(f"Slider value received: {selected_value}")
-    #cap_rate = function to get cap rate
-    #noi = function to get noi
-    #new_info = 
+    query = f"""
+            SELECT number, street 
+            FROM dtarp
+            WHERE NEIGHBORHOOD='{neighborhood}' and Total_Bedrooms = '{num_beds}' and number != '{street_number}' and street != '{street_name}'
+            """
+    
+    result = db.session.execute(text(query))
+    rows = result.fetchall()
+    all_link = []
+    for i in range(3):
+        try: 
+            address_part = f'{int(rows[i][0])} {rows[i][-1]}'
+            address_part = " ".join(address_part.split())
+            url = url_for('pick_home.home_info', address=address_part)
+            all_link.append([address_part, url])
+        except:
+            address_part = f'{int(street_number)} {street_name}'
+            address_part = " ".join(address_part.split())
+            url = url_for('pick_home.home_info', address=address_part)
+            all_link.append(["None", url])
+    
+    
     if request.method == 'POST': 
+        
         #create function to get Cap Rate and NOI 
 
         # Create a note string with the gathered data
@@ -116,7 +114,23 @@ def home_info():
         db.session.add(new_note)
         db.session.commit()
         flash('Note added!', category='success')
-    return render_template("display_home.html", user=current_user, address=address, cap_rate=cap_rate, noi=noi, monthly_rent=monthly_rent, annual_rental_income=annual_rental_income, expenses=expenses)
+        
+    return render_template(
+        "display_home.html", 
+        user=current_user, address=address, cap_rate=cap_rate, noi=noi, 
+        monthly_rent=monthly_rent, annual_rental_income=annual_rental_income, 
+        expenses=expenses, zoning=zoning, sqft=sqft, property_value=property_value, 
+        num_beds=num_beds, acres=acres, etj=etj, all_link=all_link,
+        vacancy_allowance_pct=vacancy_allowance_pct * 100, isGas=isGas,
+        num_units=num_units, utilities=utilities, legal=legal
+    )
+    # return render_template(
+        # "display_home.html", user=current_user, 
+        # address=address, cap_rate=cap_rate, noi=noi, 
+        # monthly_rent=monthly_rent, annual_rental_income=annual_rental_income, 
+        # expenses=expenses, zoning=zoning, sqft=sqft, property_value=property_value, 
+        # num_beds=num_beds, acres=acres, etj=etj, all_link=all_link
+    # )
 
 @pick_home.route('/favorites', methods=['GET', 'POST'])
 def fav():
@@ -155,6 +169,10 @@ def all_addresses():
 def get_all_addresses():
     try:
         # Query to fetch all addresses
+        # query = """
+        #     SELECT street, number, city, state, zip_code, country 
+        #     FROM dtarp
+        # """
         query = """
             SELECT street, number, NEIGHBORHOOD, CITY, PIN_EXT
             FROM dtarp
@@ -162,34 +180,39 @@ def get_all_addresses():
         result = db.session.execute(text(query))
         addresses = result.fetchall()
 
+        # Convert the query result to a list of dictionaries
+
+
         address_list = []
 
         for row in addresses:
-            street = row[0]
-            number = row[1]
-            neighborhood = row[2] or ''
-            city = row[3] or ''
+            try:
+                # Attempt to create the address part
+                address_part = f'{int(row[1])} {row[0]}'
+                address_part = " ".join(address_part.split())  # Clean up any extra spaces
 
-            if number and street:
-                address_part = f"{int(number)} {street}".strip()
-                # Generate the URL
+                # Generate the URL using url_for
                 url = url_for('pick_home.home_info', address=address_part)
+
                 # Create the link
                 link = f'<a href="{url}">View Property</a>'
-            else:
-                link = ''
+            except (TypeError, ValueError, AssertionError):
+                # If it fails (e.g., row[1] is None), set link to None
+                link = None
 
-            # Build the address dictionary
-            address_dict = {
-                'number': number or '',
-                'street': street or '',
-                'city': city,
+            # Add the address to the list
+            address = {
+                'street': row[0],
+                'city': row[3],
                 'state': 'NC',
-                'neighborhood': neighborhood,
+                'zip_code': row[2],
                 'country': 'USA',
+                'number': row[1],
                 'link': link
             }
-            address_list.append(address_dict)
+
+            address_list.append(address)
+
 
         # Return the list as a JSON response
         return jsonify(address_list)
